46837c046aebf0034054ffc045fcb4303148795b
zivcak123 <your@example.com>
2021-10-20 09:12:31 +0200

    moj prvy commit

--- public/theme/onas/index.php
+++ public/theme/onas/index.php
@@ -2,9 +2,7 @@
 	include '../../assets/hlavicka.php';
 	include '../../assets/navbar.php'
  ?>
-		<section>
-				<h1 style="color:black; text-shadow: 10px 3px 1px lightpink; text-align: center; font-size: 1500%;">o nás</h1>
-		</section>
+		
 
  <?php 
  	include '../../assets/pata.php'
